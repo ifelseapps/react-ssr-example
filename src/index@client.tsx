3bad2799292e491c3f8859bdc8727b1710a4a16e
Maxim Pavlov <ifelseapps@gmail.com>
2022-10-21 15:04:55 +0300

    Удаление данных стора из глобальной области видимости

--- src/index@client.tsx
+++ src/index@client.tsx
@@ -5,6 +5,8 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './store';
 
+delete (window as any).__STORE_INITIAL_DATA__;
+
 const container = document.getElementById('app');
 
 if (container) {
